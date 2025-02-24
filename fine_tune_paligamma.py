fc84d13d815d0d797fa267be3cadf7e8b08243c4
Cavit Erginsoy <cavit@erginsoy.com>
2025-02-24 04:55:04 +0000

    a

--- fine_tune_paligamma.py
+++ fine_tune_paligamma.py
@@ -180,7 +180,7 @@ def main():
 
     # 7. Initialize the Trainer with a custom compute_loss method
     class CustomTrainer(Trainer):
-        def compute_loss(self, model, inputs, return_outputs=False):
+        def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=None):
             outputs = model(**inputs)
             loss = outputs.loss
             return (loss, outputs) if return_outputs else loss
